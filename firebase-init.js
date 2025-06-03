667be3c1cd09bc80684789346bfce799fb8333b7
rangten <rangelfurlaneto@gmail.com>
2025-06-03 19:21:44 -0300

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+// Configuração do Firebase para Anúncio Fácil IA
+const firebaseConfig = {
+  apiKey: "AIzaSyDhTzKa989sc46gFx1QD8Tm-siN6aZzFQ",
+  authDomain: "anuncio-facil-9bc0a.firebaseapp.com",
+  projectId: "anuncio-facil-9bc0a",
+  storageBucket: "anuncio-facil-9bc0a.appspot.com",
+  messagingSenderId: "770989094257",
+  appId: "1:770989094257:web:07666e8c7558219e4f1880"
+};
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
