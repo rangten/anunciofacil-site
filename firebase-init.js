1065c2a512c160cf936b13fea6464a414d840e24
rangten <rangelfurlaneto@gmail.com>
2025-06-04 18:50:52 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,12 +1,11 @@
-// Configuração do Firebase para Anúncio Fácil IA
+// firebase-init.js
 const firebaseConfig = {
-  apiKey: "AIzaSyDhTzKa989sc46gFx1QD8Tm-siN6aZzFQ",
+  apiKey: "AIzaSyDht2RKa989sc46gFx1QD8Tm-siN6aZzFQ",
   authDomain: "anuncio-facil-9bc0a.firebaseapp.com",
   projectId: "anuncio-facil-9bc0a",
   storageBucket: "anuncio-facil-9bc0a.appspot.com",
-  messagingSenderId: "770989094257",
-  appId: "1:770989094257:web:07666e8c7558219e4f1880"
+  messagingSenderId: "770989904257",
+  appId: "1:770989904257:web:07666e8c7558219e4f1880"
 };
 
-// Inicializa o Firebase
 firebase.initializeApp(firebaseConfig);
